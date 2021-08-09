e88753f3ea18adad8ca2b0642fa9f119f9156fc6
Joaquim <49108799+lgeekj@users.noreply.github.com>
2021-08-09 21:29:50 +0100

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -5,7 +5,7 @@
 sudo add-apt-repository -y ppa:atareao/atareao
 sudo add-apt-repository -y ppa:obsproject/obs-studio
 
-sudo apt-get install alttab apt-xapian-index arandr build-essential compton compton-conf conky-all clipit dconf-editor gdebi gconf-editor hardinfo htop gedit git featherpad hardinfo grub-customizer hwinfo lshw-gtk lxsession-logout lxappearance gnome-disk-utility neofetch screenfetch net-tools nitrogen openbox openbox-dev pluma synaptic tint2 unrar rar volumeicon-alsa xarchiver xfce4-dev-tools redshift-gtk 
+sudo apt-get install -y alttab apt-xapian-index arandr build-essential compton compton-conf conky-all clipit dconf-editor gdebi gconf-editor hardinfo htop gedit git featherpad hardinfo grub-customizer hwinfo lshw-gtk lxsession-logout lxappearance gnome-disk-utility neofetch screenfetch net-tools nitrogen openbox openbox-dev pluma synaptic tint2 unrar rar volumeicon-alsa xarchiver xfce4-dev-tools redshift-gtk xfce4-panel-profiles 
 
 #sudo apt-get install devscripts dpkg-dev equivs dh-make-perl
 
