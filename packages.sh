ce118ad4af53d801711f80680d622e8e4a68b412
Joaquim <49108799+lgeekj@users.noreply.github.com>
2021-08-09 20:48:31 +0100

    Create packages.sh

---
+++ packages.sh
@@ -0,0 +1,8 @@
+##### packages for ubuntu 
+##### execute as root or with sudo
+
+sudo add-apt-repository -y ppa:atareao/atareao
+sudo add-apt-repository -y ppa:obsproject/obs-studio
+
+sudo apt-get install alttab apt-xapian-index arandr build-essential compton compton-conf conky-all clipit dconf-editor gdebi gconf-editor hardinfo htop gedit git featherpad hardinfo grub-customizer hwinfo lshw-gtk lxsession-logout lxappearance gnome-disk-utility neofetch screenfetch net-tools nitrogen openbox openbox-dev pluma synaptic tint2 unrar rar volumeicon-alsa xarchiver xfce4-dev-tools
+
